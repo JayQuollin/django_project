08b750019c0c6b910656ecc00851af41c327bcfc
Julia Gorb <ulia.gor.2010@yandex.ru>
2026-05-30 11:33:23 +0300

    третий урок

--- blog_app/models.py
+++ blog_app/models.py
@@ -3,15 +3,38 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, verbose_name="Название категории")
     slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['id']
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, verbose_name="Заголовок статьи")
     slug = models.SlugField(unique=True)
-    content = models.TextField()
-    published = models.BooleanField(default=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE) # поле автор - внешний ключ на другую таблицу
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField(verbose_name="Текст статьи")
+    published = models.BooleanField(default=False, verbose_name="Статус публикации")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор") # поле автор - внешний ключ на другую таблицу
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="posts",
+        verbose_name="Категория"
+    )
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+        ordering = ['-created_at']
+
+    def increase_views_count(self):
+        self.increase_views_count += 1
+        self.save()
+
+    def __str__(self):
+        return f'Пост: {self.title}'
